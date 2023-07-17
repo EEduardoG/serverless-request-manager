7470db5492d5416b7e4b22aba21586da7efb5f46
EEduardoG <85304409+EEduardoG@users.noreply.github.com>
2023-07-17 11:42:49 -0500

    export exceptioncode enum

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import RequestService from "./request_service/RequestService";
 import ResponseService from "./response_service/ResponseService";
 import { IException } from "./interfaces/IException";
 import { IResponse } from "./interfaces/IResponse";
-
+import { ExceptionCode } from "./interfaces/IException";
 
 export {
     RequestService,
     ResponseService,
     IException,
-    IResponse
+    IResponse,
+    ExceptionCode
 }
